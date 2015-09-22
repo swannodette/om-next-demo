8b837116ae831a051163729128df470b82a62029
David Nolen <david.nolen@gmail.com>
2015-09-22 18:49:57 -0500

    wip

--- todomvc/src/cljs/todomvc/util.cljs
+++ todomvc/src/cljs/todomvc/util.cljs
@@ -1,4 +1,7 @@
-(ns todomvc.util)
+(ns todomvc.util
+  (:require [cljs.core.async :refer [chan]]
+            [cognitect.transit :as t])
+  (:import [goog.net XhrIo]))
 
 (defn hidden [is-hidden]
   (if is-hidden
@@ -9,3 +12,12 @@
   (if (== n 1)
     word
     (str word "s")))
+
+(defn transit-post [url edn]
+  (let [c (chan)]
+    (.send XhrIo url "POST"
+      (fn [cb res]
+        (put! c (t/read (t/reader :json) res)))
+      (t/write (t/writer :json) edn)
+      #js {"Content-Type" "application/transit+json"})
+    c))
