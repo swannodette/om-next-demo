688a49ed26970bc13825c6e406c8cc6b7061aebe
dnolen <dnolen@cognitect.com>
2015-10-14 21:00:11 -0400

    demonstrate that updates from server don't yet work

--- todomvc/src/cljs/todomvc/parser.cljs
+++ todomvc/src/cljs/todomvc/parser.cljs
@@ -36,9 +36,10 @@
 (defmethod mutate 'todo/update
   [{:keys [state ref]} _ new-props]
   {:remote true
-   :action ;; OPTIMISTIC UPDATE
-   (fn []
-     (swap! state update-in ref merge new-props))})
+   ;:action ;; OPTIMISTIC UPDATE
+   ;(fn []
+   ;  (swap! state update-in ref merge new-props))
+   })
 
 (defmethod mutate 'todo/edit
   [{:keys [state]} _ {:keys [db/id]}]
