9abe4cdfc28569b4479a24f6d5530dcf398da13c
David Nolen <david.nolen@gmail.com>
2015-10-16 08:07:38 -0400

    debugging builds

--- todomvc/script/build.clj
+++ todomvc/script/build.clj
@@ -6,8 +6,19 @@
    :optimizations      :simple
    :static-fns         true
    :optimize-constants true
+   :pretty-print       true
    :externs            ["src/js/externs.js"]
    :closure-defines    '{goog.DEBUG false}
    :verbose            true})
+
+#_(b/build "src/cljs"
+ {:output-to       "resources/public/js/app.js"
+  :output-dir      "resources/public/js"
+  :optimizations   :advanced
+  :pretty-print    true
+  :pseudo-names    true
+  :externs         ["src/js/externs.js"]
+  :closure-defines '{goog.DEBUG false}
+  :verbose          true})
 
 (System/exit 0)
