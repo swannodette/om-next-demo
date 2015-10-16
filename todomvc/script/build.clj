d17736d04281678506ec87d6593c92cc50c5e9a2
David Nolen <david.nolen@gmail.com>
2015-10-15 21:33:46 -0400

    debug prod build

--- todomvc/script/build.clj
+++ todomvc/script/build.clj
@@ -3,6 +3,8 @@
 (b/build "src/cljs"
   {:output-to     "resources/public/js/app.js"
    :optimizations :advanced
+   :pretty-print  true
+   :pseudo-names  true
    :output-dir    "resources/public/js"
    :verbose       true})
 
