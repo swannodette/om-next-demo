01e56abfa5cde3b5c2869bf9f3623f65371fe1a2
dnolen <dnolen@cognitect.com>
2015-11-04 18:20:34 -0500

    tweaks

--- todomvc/project.clj
+++ todomvc/project.clj
@@ -7,10 +7,10 @@
   :jvm-opts ^:replace ["-Xms512m" "-Xmx512m" "-server"]
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.145"]
+                 [org.clojure/clojurescript "1.7.169"]
                  [com.datomic/datomic-free "0.9.5206"]
                  [bidi "1.20.3"]
-                 [org.omcljs/om "1.0.0-alpha3"]
+                 [org.omcljs/om "1.0.0-alpha15-SNAPSHOT"]
                  [ring/ring "1.4.0"]
                  [com.cognitect/transit-clj "0.8.281"]
                  [com.cognitect/transit-cljs "0.8.225"]
