a0c4f014f39de7d65158eec8b1e52b4e5da3bf48
António Nuno Monteiro <anmonteiro@gmail.com>
2016-01-13 23:47:23 +0100

    update deps, add clean targets

--- todomvc/project.clj
+++ todomvc/project.clj
@@ -6,15 +6,16 @@
 
   :jvm-opts ^:replace ["-Xms512m" "-Xmx512m" "-server"]
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
-                 [com.datomic/datomic-free "0.9.5206"]
-                 [bidi "1.20.3"]
-                 [org.omcljs/om "1.0.0-alpha20-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC5"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [com.datomic/datomic-free "0.9.5344"]
+                 [bidi "1.25.0"]
+                 [org.omcljs/om "1.0.0-alpha29-SNAPSHOT"]
                  [ring/ring "1.4.0"]
-                 [com.cognitect/transit-clj "0.8.281"]
-                 [com.cognitect/transit-cljs "0.8.225"]
-                 [com.stuartsierra/component "0.2.3"]]
+                 [com.cognitect/transit-clj "0.8.285"]
+                 [com.cognitect/transit-cljs "0.8.237"]
+                 [com.stuartsierra/component "0.3.1"]
 
+  :clean-targets ^{:protect false} ["resources/public/js"]
   :source-paths ["src/clj" "src/cljs" "src/dev"]
   )
